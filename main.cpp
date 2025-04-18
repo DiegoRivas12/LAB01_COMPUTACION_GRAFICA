c7ae9f118bffd36f4d060ba2b49c4ce597c4c09e
DiegoRivas1 <rhyi888@gmail.com>
2025-04-17 23:45:37 -0500

    Verificacion del rango pixel de 0 a 255

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #define N 3 //Numero de canales
-
+std::string rangoPixel(cv::Mat canal){//Verifica que el piel este en el rango de 0 a 255
+    
+    for(int row=0;row<canal.rows;row++){
+        for(int col=0;col<canal.cols;col++){
+            int valorPixel=canal.at<uchar>(row,col);
+            if((valorPixel<=255 && valorPixel>=0)==false)
+                return "Fuera de rango de 0 a 255";//std::cout<<"Pixel fuera de rango"<<std::endl;
+        }
+    }
+    return "Dentro del rango de 0 a 255";
+    
+}
 cv::Mat* extraerCanal(cv::Mat img){
     cv::Mat* RGB = new cv::Mat[N];
     cv::split(img, RGB);
@@ -10,20 +21,24 @@ cv::Mat* extraerCanal(cv::Mat img){
 }
 int main() {
     // Ruta de la imagen
-    std::string imagePath = "dado.png";  // Cambia esta ruta por la ubicación de tu imagen
+    std::string imagePath = "dado.png"; 
     cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
     cv::Mat* BGR = extraerCanal(image);
 
     const char* nombreCanales[N]={"Azul","Verde","Rojo"};
-
+    cv::imshow("Imagen Original",image);
+    cv::resizeWindow("Imagen Original", 350, 350);
     for(int i=0;i<N;i++){
+        std::cout<<"Canal "<<nombreCanales[i]<<" "<<rangoPixel(BGR[i]);
+        std::cout<<std::endl;
         cv::imshow(nombreCanales[i],BGR[i]);
         cv::resizeWindow(nombreCanales[i], 350, 350);
+        
     }
     cv::waitKey(0);
-    delete[] BGR;  // Libera la memoria que fue asignada con new
+    delete[] BGR; 
 
-    cv::destroyAllWindows();  // Cierra todas las ventanas
+    cv::destroyAllWindows();  
 
     return 0;
 }
